598300579b894d5780269ebd535c3c727edbaf2f
hoylemd <hoylemd@gmail.com>
2011-04-11 00:18:32 -0400

    added tiny language example files.

---
+++ example/c_tiny/tinyl.h
@@ -0,0 +1,12 @@
+#ifndef TINYL_H
+#define TINYL_H
+
+#include "globals.h"
+#include "util.h"
+#include "scan.h"
+#include "y.tab.h"
+
+/* lexeme of identifier or reserved word */
+char tokenString[MAXTOKENLEN+1];
+
+#endif
